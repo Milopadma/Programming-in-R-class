19b2826138c91ab1d81ed246db0e4ad2b245ea9b
mylo <48256865+Milopadma@users.noreply.github.com>
2022-06-24 15:11:57 +0800

    testing functions

--- first.r
+++ first.r
@@ -18,3 +18,16 @@ testing <- function() {
   lines(line2, col = "red", type = "l", lwd = 2)
 }
 testing()
+
+
+# a function that takes in a list of numbers
+# and returns the sum of all the numbers in the list
+list1 <- list(1, 2, 3, 4, 5)
+sumoflist <- function(list) {
+  sum <- 0
+  for (i in list) {
+    sum <- sum + i
+  }
+  return(sum)
+}
+sumoflist(list1)
